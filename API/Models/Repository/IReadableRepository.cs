e11c2159216928e6f0fb6560e0ea6635f4cdb53d
Pytokaa <elioth.rob@proton.me>
2025-11-10 18:05:12 +0100

    interface repository

--- API/Models/Repository/IReadableRepository.cs
+++ API/Models/Repository/IReadableRepository.cs
@@ -1,6 +1,7 @@
 namespace API.Models.Repository;
 
-public interface IReadableRepository
+public interface IReadableRepository<TEntity, in TIdentifier>
 {
-    
+    Task<IEnumerable<TEntity>> GetAllAsync();
+    Task<TEntity?> GetByIdAsync(TIdentifier id);
 }
