dc3c2c82cda663b8bf3ed9654e9424dd226f9d10
Stapleton Alexis <155957705+AlexiStapleton@users.noreply.github.com>
2025-11-14 14:32:23 +0100

    amelioration du compte

--- API/Controllers/LoginController.cs
+++ API/Controllers/LoginController.cs
@@ -1,9 +1,11 @@
+using System.ComponentModel.DataAnnotations;
 using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.IdentityModel.Tokens;
 using System.IdentityModel.Tokens.Jwt;
 using System.Security.Claims;
 using System.Text;
+using System.Text.RegularExpressions;
 using API.Models.EntityFramework;
 using API.Models.Repository;
 
@@ -13,6 +15,7 @@ public class LoginRequest
     public string? Login { get; set; }
     public string? Email { get; set; }
     public string Password { get; set; }
+    public string? PasswordConfirm { get; set; }
 }
 [Route("api/[controller]")]
 [ApiController]
@@ -28,22 +31,22 @@ public class LoginController : ControllerBase
         _dataRepository = dataRepo;
     }
 
-    [HttpGet]
-    public async Task<IActionResult> GetUtilisateurs()
-    {
-        var actionResult = await _dataRepository.GetAllAsync();
-        if (actionResult == null)
-        {
-            return BadRequest();       
-        }
-
-        _utilisateurs = actionResult.ToList();
-        if (_utilisateurs.Count == 0)
-        {
-            return BadRequest();
-        }
-        return Ok(_utilisateurs);
-    }
+    // [HttpGet]
+    // public async Task<IActionResult> GetUtilisateurs()
+    // {
+    //     var actionResult = await _dataRepository.GetAllAsync();
+    //     if (actionResult == null)
+    //     {
+    //         return BadRequest();
+    //     }
+    //
+    //     _utilisateurs = actionResult.ToList();
+    //     if (_utilisateurs.Count == 0)
+    //     {
+    //         return BadRequest();
+    //     }
+    //     return Ok(_utilisateurs);
+    // }
 
     [HttpPost]
     [AllowAnonymous]
@@ -80,6 +83,67 @@ public class LoginController : ControllerBase
             });
         }
         return response;
+    }
+    
+    [HttpPost("signup")]
+    public async Task<IActionResult> SignUp([FromBody] LoginRequest request)
+    {
+        if (request == null || string.IsNullOrEmpty(request.Email) ||
+            string.IsNullOrEmpty(request.Login) || string.IsNullOrEmpty(request.Password) || string.IsNullOrEmpty(request.PasswordConfirm))
+        {
+            return BadRequest("Données invalides.");
+        }
+        
+        if (!new EmailAddressAttribute().IsValid(request.Email))
+        {
+            return BadRequest("Email invalide.");
+        }
+        
+        string pattern = @"^(?=.*[a-z])(?=.*[A-Z])(?=.*\d)(?=.*[@$!%*?&])[A-Za-z\d@$!%*?&]{8,}$";
+        if (!Regex.IsMatch(request.Password, pattern))
+        {
+            return BadRequest("Le mot de passe doit contenir au moins 8 caractères, une majuscule, une minuscule, un chiffre et un caractère spécial.");
+        }
+
+        if (request.Password != request.PasswordConfirm)
+        {
+            return BadRequest("Les mots de passe ne correspondent pas");
+        }
+
+        var existingUsers = await _dataRepository.GetAllAsync();
+        if (existingUsers.Any(u => u.Email.ToUpper() == request.Email.ToUpper()))
+            return BadRequest("Cet email est déjà utilisé.");
+
+        if (existingUsers.Any(u => u.Login.ToUpper() == request.Login.ToUpper()))
+            return BadRequest("Ce login est déjà utilisé.");
+
+        // Créer le nouvel utilisateur
+        var newUser = new Utilisateur
+        {
+            Email = request.Email,
+            Login = request.Login,
+            Password = BCrypt.Net.BCrypt.HashPassword(request.Password),
+            Description = "",
+            StatutId = 1,
+            Dateinscription = DateTime.UtcNow
+        };
+
+        await _dataRepository.AddAsync(newUser);
+
+        var tokenString = GenerateJwtToken(newUser);
+
+        return Ok(new
+        {
+            message = "Inscription réussie.",
+            token = tokenString,
+            userDetails = new
+            {
+                newUser.UtilisateurId,
+                newUser.Email,
+                newUser.Login,
+                newUser.Dateinscription
+            }
+        });
     }
 
     private Utilisateur AuthentificateUtilisateur(string login, string password)
