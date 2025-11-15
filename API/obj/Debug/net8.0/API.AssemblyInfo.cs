5f2294df94a02ba7bc0711398d2cf2b060714507
Pytokaa <elioth.rob@proton.me>
2025-11-16 00:18:16 +0100

    ajout du projet

--- API/obj/Debug/net8.0/API.AssemblyInfo.cs
+++ API/obj/Debug/net8.0/API.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("API")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+85dfbd5f7592c4e01fbc881a1b95de5f04d0e018")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+5f1c05b84dc5d0c746b0353a254352fe31e0e59b")]
 [assembly: System.Reflection.AssemblyProductAttribute("API")]
 [assembly: System.Reflection.AssemblyTitleAttribute("API")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
