6f047c694b6cd96c33e56a74a933fe704169de05
Andre SUZUKI-SANCHES-MELLO <suzukisa@iut-acy.local>
2025-12-04 11:37:06 +0100

    new file:   FrontBlazor/Attributes/IgnoreInTemplateAttribute.cs 	modified:   FrontBlazor/Models/Annonces/Annonce.cs 	new file:   FrontBlazor/Models/EtatArticle/EtatArticle.cs 	new file:   FrontBlazor/Models/Tag/Tag.cs 	modified:   FrontBlazor/Pages/AddArticle.razor

---
+++ FrontBlazor/Models/Tag/Tag.cs
@@ -0,0 +1,15 @@
+﻿using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace FrontBlazor.Models;
+public class Tag : IEntity
+{
+    public int TagId { get; set; }
+
+    public string? LibelleTag { get; set; } = null;
+
+    public int GetId()
+    {
+        return TagId;
+    }
+}
