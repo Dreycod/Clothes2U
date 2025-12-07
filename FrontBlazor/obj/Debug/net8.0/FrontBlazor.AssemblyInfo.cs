16d5d9e771f9a509ba57ca15b4bb82266c865f5a
Drey <84332780+Dreycod@users.noreply.github.com>
2025-12-07 16:28:20 +0100

    ProductDetail bug (mis VM_Search, faut fix)

--- FrontBlazor/obj/Debug/net8.0/FrontBlazor.AssemblyInfo.cs
+++ FrontBlazor/obj/Debug/net8.0/FrontBlazor.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("FrontBlazor")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+0d996e76bf00e2575d526dac1c19af9d4502953c")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+c88325fae0fb6b98973bcd4ace1e36f701ebe5d5")]
 [assembly: System.Reflection.AssemblyProductAttribute("FrontBlazor")]
 [assembly: System.Reflection.AssemblyTitleAttribute("FrontBlazor")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
