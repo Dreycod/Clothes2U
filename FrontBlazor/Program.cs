ab8a3f20fe04842a422959ccef0c343d9ad4dccd
Drey <84332780+Dreycod@users.noreply.github.com>
2025-12-08 15:06:26 +0100

    Bug fix tailleservice

--- FrontBlazor/Program.cs
+++ FrontBlazor/Program.cs
@@ -23,7 +23,6 @@ builder.Services.AddScoped<IAuthService, AuthService>();
 builder.Services.AddScoped<ICategorieService<Categorie>, CategorieService>();
 builder.Services.AddScoped<IConversationService<Conversation>, ConversationService>();
 builder.Services.AddScoped<IMessageService<Message>, MessageService>();
-builder.Services.AddScoped<ITailleService<Taille>, TailleService>();
 builder.Services.AddScoped<IMarqueService<Marque>, MarqueService>();
 builder.Services.AddScoped<ICouleurService<Couleur>, CouleurService>();
 builder.Services.AddScoped<IFavorisService<Favoris>, FavorisService>();
