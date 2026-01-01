1f11102e597a133fd2912bf8c8aa4758d1ccad3c
TorduGenial <faher.mohamed.pro@gmail.com>
2026-01-02 00:18:10 +0100

    Ajout de l'historique des achats/ventes (côté FRONT)

--- FrontBlazor/ViewModel/BaseViewModel.cs
+++ FrontBlazor/ViewModel/BaseViewModel.cs
@@ -5,9 +5,12 @@ namespace FrontBlazor.ViewModel;
 
 public abstract class BaseViewModel
 {
-    private readonly IAuthService  _authService;
-    private readonly NavigationManager _nav;
-    
+    protected readonly IAuthService  _authService;
+    protected readonly NavigationManager _nav;
+
+    public event Action? OnStateChanged;
+
+
     public BaseViewModel(
         IAuthService authService,
         NavigationManager nav
@@ -17,6 +20,10 @@ public abstract class BaseViewModel
         _nav = nav;
     }
 
+    protected void NotifyStateChanged()
+    {
+        OnStateChanged?.Invoke();
+    }
 
     public async Task VerifiyAccountAsync()
     {
