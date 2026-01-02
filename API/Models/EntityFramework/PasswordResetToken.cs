145510d2c4151ca3083f40dcf80a223af0ba16c3
TorduGenial <faher.mohamed.pro@gmail.com>
2026-01-02 01:09:35 +0100

    Ajout de l'entité pour le mot de passe oublié.

---
+++ API/Models/EntityFramework/PasswordResetToken.cs
@@ -0,0 +1,31 @@
+﻿using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace API.Models.EntityFramework
+{
+    [Table("t_e_password_reset")]
+    public class PasswordResetToken : IEntity
+    {
+        [Key]
+        [Column("reset_id")]
+        public int PasswordResetTokenId { get; set; }
+
+        [Column("reset_token")]
+        public string Token { get; set; }
+
+        [Column("reset_expiration")]
+        public DateTime Expiration { get; set; }
+
+        [Column("reset_used")]
+        public bool Used { get; set; }
+
+        [Column("reset_utilisateur_id")]
+        public int UtilisateurId { get; set; }
+
+        [ForeignKey(nameof(UtilisateurId))]
+        [InverseProperty(nameof(Utilisateur.PasswordResetTokens))]
+        public virtual Utilisateur UtilisateurReset { get; set; }
+
+        public int GetId() => PasswordResetTokenId;
+    }
+}
