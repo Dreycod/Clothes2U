49a1a2ade293477856a306ae79edea32a4de820e
Akiel <122918848+Akieldamas@users.noreply.github.com>
2026-01-10 19:22:25 +0100

    changement à home avec image, text, pfps sur annonces

--- API/Mapper/AnnonceMappingProfile.cs
+++ API/Mapper/AnnonceMappingProfile.cs
@@ -31,7 +31,7 @@ public class AnnonceMappingProfile : Profile
             .ForMember(dest => dest.Prix, opt => opt.MapFrom(src => src.Prix))
             .ForMember(dest => dest.NomAuteur, opt => opt.MapFrom(src => src.Utilisateur.Login))
             .ForMember(dest => dest.IdAuteur, opt => opt.MapFrom(src => src.UtilisateurId))
-            .ForMember(dest => dest.UriPhotoProfilAuteur,
+            .ForMember(dest => dest.IdPhotoProfilAuteur,
                 opt => opt.MapFrom(src => src.Utilisateur.PhotoProfil.PhotoId))
             .ReverseMap();
         
