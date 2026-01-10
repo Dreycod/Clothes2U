9cc12b441c3ae3068dc51f58cb115071382f933a
TorduGenial <faher.mohamed.pro@gmail.com>
2026-01-10 20:30:23 +0100

    Ajout de l'injection de dépendence d'historisation de l'utilisateur dans le Back

--- API/Program.cs
+++ API/Program.cs
@@ -19,6 +19,7 @@ using System.Text.Json.Serialization;
 using API.Models.Repository.Interfaces;
 using Shared.DTO.Photo;
 using Shared.DTO.Tag;
+using API.Services.Interfaces;
 
 var builder = WebApplication.CreateBuilder(args);
 
@@ -274,7 +275,7 @@ builder.Services.AddScoped<IAnnonceExtensionService, AnnonceExtensionService>();
 builder.Services.AddHttpContextAccessor();
 builder.Services.AddScoped<INotificationMailService, NotificationMailService>();
 builder.Services.AddScoped<PasswordResetService>();
-
+builder.Services.AddScoped<IUserDeletionService, UserDeletionService>();
 
 //notification
 builder.Services.AddScoped<INotificationRepository, NotificationManager>();
